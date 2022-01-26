de852e00bb923887b633ed1b0fa904b1fda3a0b0
zhangxinyu <zhangxinyu@beyondcent.com>
2022-01-26 18:13:03 +0800

    es6 - 1月26日

--- src/js-export/m1.js
+++ src/js-export/m1.js
@@ -1,3 +1,4 @@
+//分别暴露
 export let school = '尚硅谷';
 export function teach(){
     console.log("我可以教你开发技能");
---
+++ src/js-export/m3.js
@@ -0,0 +1,7 @@
+//默认暴露
+export default{
+    school:'GUIGU',
+    change:function(){
+        console.log('我们可以改变你');
+    }
+}
